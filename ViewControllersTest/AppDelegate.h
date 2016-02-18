f21f7426fe3e7759d192eb41f78b8af8cb5b13a1
EKravtz32 <ekravtz32@ekravtz32s-imac.local>
2016-02-18 20:49:40 +0200

    Initial Commit

---
+++ ViewControllersTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewControllersTest
+//
+//  Created by EKravtz32 on 18.02.16.
+//  Copyright © 2016 EvgenKravtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewControllersTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewControllersTest
+//
+//  Created by EKravtz32 on 18.02.16.
+//  Copyright © 2016 EvgenKravtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
